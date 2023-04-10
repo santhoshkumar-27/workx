f5186b7e0620c0e70aefed24a9e8aadf405885c7
kaviyarasan <kaviyarasan.t@twilightitsolutions.com>
2023-04-10 16:48:07 +0530

    fix: changes in the exhaust map

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,8 +19,17 @@ export class AppComponent implements OnInit {
   ngOnInit(): void {
     this.post$.pipe(
       tap(value => console.log('value', value)),
+      /*
+        switchMap cancel any inner stream doing actively, when the higher order observable emitted new value, the inner stream get cancels
+      */
       // switchMap(innerStream => this.dbjson.getById(innerStream.id))
+      /*
+        concatMap waits until a inner stream get finished, when the higher order observable emitted new value;
+      */
       // concatMap(innerStream => this.dbjson.getById(innerStream.id))
+      /*
+        exhaustMap cancels the higher order observable value emitted, when the inner stream yet to be finished;
+      */
       exhaustMap(innerStream => this.dbjson.getById(innerStream.id))
     ).subscribe((res: any) => console.log('response', res));
   }
